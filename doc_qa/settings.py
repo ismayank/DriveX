b9d86c9c45e527a40e502a37d826d9dccefd2563
ismayank <mayanksinghchakulia@gmail.com>
2025-01-10 22:17:02 +0530

    first commit1

--- doc_qa/settings.py
+++ doc_qa/settings.py
@@ -17,25 +17,12 @@ import os
 
 load_dotenv()  # Load environment variables
 
-# Verify the key is loaded
-TEXTRAZOR_API_KEY = os.getenv("TEXTRAZOR_API_KEY")
-print(f"Loaded TEXTRAZOR_API_KEY: {TEXTRAZOR_API_KEY}")
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bj)*oouf&=nhem4^!%hsvw1&$rylpjjvr2yi-0!h3_3j9k@6f6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drive-x-ozqe.vercel.app', 'localhost']
 
 
 # Application definition
